ebe16cba4091b69472ac55dbfb66afc34f8d03f6
Romeo Atoyan <atoyanr@gmail.com>
2023-04-17 15:37:03 +0200

    navbar options are more cleaner and  reusable

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,98 +1,45 @@
-// import React, { useState } from "react";
-// import MovieGenres from "../MovieRequests";
-// import "./Navbar.css";
-
-// const Navbar = ({ setSelectedCategory, selectedCategory }) => {
-//   const [toggleMovieGenres, setToggleMovieGenres] = useState(false);
-//   const [toggleTVGenres, setToggleTVGenres] = useState(false);
-
-//   const handleMovieToggle = () => {
-//     setToggleMovieGenres(!toggleMovieGenres);
-//     setToggleTVGenres(false);
-//   };
-
-//   const handleGenre = (genre) => {
-//     setSelectedCategory(genre);
-//     setToggleMovieGenres(!toggleMovieGenres)
-//     setToggleTVGenres(!toggleTVGenres)
-//   };
-
-//   const handleTvToggle = () => {
-//     setToggleTVGenres(!toggleTVGenres);
-//   };
-
-//   return (
-//     <nav className="navbar_container">
-//       <div className="navbar">
-//         <h1 style={{ cursor: "pointer" }}>
-//           Cine<span style={{ color: "yellowgreen" }}>Hub</span>
-//         </h1>
-//         <div className="navbar_categories">
-//           <div className="navbar_categories_movie">
-//             <h4 onClick={handleMovieToggle}>Movies</h4>
-//             {toggleMovieGenres ? (
-//               <div className="dropdown-movie">
-//                 <p onClick={() => handleGenre("TopRated")}>Top Rated</p>
-//                 <p onClick={() => handleGenre("Action")}>Action</p>
-//                 <p onClick={() => handleGenre("Adventure")}>Adventure</p>
-//                 <p onClick={() => handleGenre("Comedy")}>Comedy</p>
-//                 <p onClick={() => handleGenre("Thriller")}>Thriller</p>
-//                 <p onClick={() => handleGenre("Romance")}>Romance</p>
-//                 <p onClick={() => handleGenre("SciFi")}>SciFi</p>
-//                 <p onClick={() => handleGenre("Crime")}>Crime</p>
-//                 <p onClick={() => handleGenre("Drama")}>Drama</p>
-//                 <p onClick={() => handleGenre("Popular")}>Popular</p>
-//               </div>
-//             ) : (
-//               ""
-//             )}
-//           </div>
-//           <div className="navbar_categories_tv-show">
-//             <h4 onClick={handleTvToggle}>Tv Show</h4>
-//             {toggleTVGenres ? (
-//               <div className="dropdown-tv">
-//                 <p onClick={() => handleGenre("TVBest")}>Best</p>
-//                 <p onClick={() => handleGenre("TVPopular")}>Popular</p>
-//                 <p onClick={() => handleGenre("TVOnAir")}>On Air</p>
-//               </div>
-//             ) : (
-//               ""
-//             )}
-//           </div>
-//         </div>
-//       </div>
-//     </nav>
-//   );
-// };
-
-// export default Navbar;
-
 import React, { useState } from "react";
-import MovieGenres from "../MovieRequests";
 import "./Navbar.css";
 
-const Navbar = ({ setSelectedCategory, selectedCategory }) => {
-  const [toggleMovieGenres, setToggleMovieGenres] = useState(false);
-  const [toggleTVGenres, setToggleTVGenres] = useState(false);
+const DropdownMenu = ({ options, handleOptionClick }) => {
+  return (
+    <div className="dropdown-menu">
+      {options.map((option) => (
+        <p key={option} onClick={() => handleOptionClick(option)}>
+          {option}
+        </p>
+      ))}
+    </div>
+  );
+};
 
-  const handleMovieToggle = () => {
-    setToggleMovieGenres(!toggleMovieGenres);
-    setToggleTVGenres(false);
+const Navbar = ({ setSelectedCategory }) => {
+  const [toggleGenres, setToggleGenres] = useState(false);
+
+  const handleToggleGenres = () => {
+    setToggleGenres(!toggleGenres);
   };
 
   const handleGenre = (genre, isMovie) => {
     setSelectedCategory(genre);
-    if (isMovie) {
-      setToggleMovieGenres(false);
-    } else {
-      setToggleTVGenres(false);
-    }
+    setToggleGenres(false);
   };
 
-  const handleTvToggle = () => {
-    setToggleTVGenres(!toggleTVGenres);
-    setToggleMovieGenres(false);
-  };
+  const movieGenres = [
+    "NetflixOriginals",
+    "TopRated",
+    "Action",
+    "Adventure",
+    "Comedy",
+    "Thriller",
+    "Romance",
+    "SciFi",
+    "Crime",
+    "Drama",
+    "Popular",
+  ];
+
+  const tvGenres = ["NetflixOriginals", "HBO", "Best", "Popular", "On Air"];
 
   return (
     <nav className="navbar_container">
@@ -102,42 +49,21 @@ const Navbar = ({ setSelectedCategory, selectedCategory }) => {
         </h1>
         <div className="navbar_categories">
           <div className="navbar_categories_movie">
-            <h4 onClick={handleMovieToggle}>Movies</h4>
-            {toggleMovieGenres ? (
-              <div className="dropdown-movie">
-                <p onClick={() => handleGenre("NetflixOriginals", true)}>
-                  Netflix
-                </p>
-                <p onClick={() => handleGenre("HBO", true)}>HBO</p>
-                <p onClick={() => handleGenre("TopRated", true)}>Top Rated</p>
-                <p onClick={() => handleGenre("Action", true)}>Action</p>
-                <p onClick={() => handleGenre("Adventure", true)}>Adventure</p>
-                <p onClick={() => handleGenre("Comedy", true)}>Comedy</p>
-                <p onClick={() => handleGenre("Thriller", true)}>Thriller</p>
-                <p onClick={() => handleGenre("Romance", true)}>Romance</p>
-                <p onClick={() => handleGenre("SciFi", true)}>SciFi</p>
-                <p onClick={() => handleGenre("Crime", true)}>Crime</p>
-                <p onClick={() => handleGenre("Drama", true)}>Drama</p>
-                <p onClick={() => handleGenre("Popular", true)}>Popular</p>
-              </div>
-            ) : (
-              ""
+            <h4 onClick={handleToggleGenres}>Movies</h4>
+            {toggleGenres && (
+              <DropdownMenu
+                options={movieGenres}
+                handleOptionClick={(option) => handleGenre(option, true)}
+              />
             )}
           </div>
           <div className="navbar_categories_tv-show">
-            <h4 onClick={handleTvToggle}>Tv Show</h4>
-            {toggleTVGenres ? (
-              <div className="dropdown-tv">
-                <p onClick={() => handleGenre("NetflixOriginals", false)}>
-                  Netflix
-                </p>
-                <p onClick={() => handleGenre("HBO", true)}>HBO</p>
-                <p onClick={() => handleGenre("TVBest", false)}>Best</p>
-                <p onClick={() => handleGenre("TVPopular", false)}>Popular</p>
-                <p onClick={() => handleGenre("TVOnAir", false)}>On Air</p>
-              </div>
-            ) : (
-              ""
+            <h4 onClick={handleToggleGenres}>TV Shows</h4>
+            {toggleGenres && (
+              <DropdownMenu
+                options={tvGenres}
+                handleOptionClick={(option) => handleGenre(option, false)}
+              />
             )}
           </div>
         </div>
